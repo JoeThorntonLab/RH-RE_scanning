3717a53d83ba02a566a74fbdc93a892ae510faa5
Jaeda Patton <jejpatton@uchicago.edu>
2023-04-24 10:46:00 -0500

    general functions for working with the RH-RE DMS data

---
+++ scripts/general_functions.R
@@ -0,0 +1,42 @@
+# general functions for working with the RH-RE DMS data
+
+# make factors with different levels for ordering of REs in plotting
+REs <- list()
+REs[[1]] <- factor(c("SRE1 (AA)", "SRE2 (GA)", "ERE (GT)", "AC", 
+                     "AG", "AT", "CA", "CC", 
+                     "CG", "CT", "GC", "GG", 
+                     "TA", "TC", "TG", "TT"), 
+                   levels=c("ERE (GT)", "SRE1 (AA)", "SRE2 (GA)", "AC", 
+                            "AG", "AT", "CA", "CC", 
+                            "CG", "CT","GC", "GG", 
+                            "TA", "TC", "TG", "TT"))
+REs[[2]] <- factor(REs[[1]], 
+                   levels=c("SRE1 (AA)", "SRE2 (GA)", "AT", "ERE (GT)",
+                            "CA", "TA", "CT", "TT",
+                            "AC", "GC", "AG", "GG",
+                            "CC", "TC", "CG", "TG"))
+REs[[3]] <- factor(REs[[1]], 
+                   levels=c("SRE1 (AA)", "CA", "AC", "CC",
+                            "SRE2 (GA)", "TA", "GC", "TC",
+                            "AT", "CT", "AG", "CG",
+                            "ERE (GT)", "TT", "GG", "TG"))
+REs[[4]] <- factor(REs[[1]], 
+                   levels=c("SRE1 (AA)", "CA", "AC", "CC",
+                            "TC", "GC", "TA", "SRE2 (GA)",
+                            "AT", "CT", "AG", "CG",
+                            "TG", "GG", "TT", "ERE (GT)"))
+REs[[5]] <- factor(REs[[1]], 
+                   levels=c("SRE1 (AA)", "SRE2 (GA)", "TA", "CA",
+                            "CC", "TC", "GC", "AC",
+                            "ERE (GT)", "TT", "CT", "AT",
+                            "TG", "GG", "AG", "CG"))
+
+# convert REBC number to RE variant
+REBC_to_RE <- function(REBC, levels=1) {
+  if(is.character(REBC)) {
+    REBC <- sub("AncSR._REBC", "", REBC)
+    REBC <- as.integer(REBC)
+  }
+  RE <- REs[[levels]][REBC]
+  return(RE)
+}
